cb80698a276fd5137572db5282bff0cfd166bfdb
WagnerReis <wagnerreis1997@gmail.com>
2023-10-13 14:34:16 +0000

    feat: add abstraction for in memory repository

--- src/category/domain/category.repository.ts
+++ src/category/domain/category.repository.ts
@@ -1,1 +1,5 @@
-export interface CategoryRepository {}
+import { IRepository } from "../../shared/domain/repository/repository.interface";
+import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { Category } from "./category.entity";
+
+export interface ICategoryRepository extends IRepository<Category, Uuid> {}
