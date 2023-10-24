4aaa6cdf759762db60b12eec5085493de8e33eec
WagnerReis <wagnerreis1997@gmail.com>
2023-10-24 00:57:48 +0000

    feat: add custom search method in in-memory repository

--- src/shared/infra/db/in-memory/in-memory.repository.ts
+++ src/shared/infra/db/in-memory/in-memory.repository.ts
@@ -1,10 +1,14 @@
 import { Entity } from "../../../domain/entity";
 import { NotFoundError } from "../../../domain/errors/not-found.error";
-import { IRepository } from "../../../domain/repository/repository.interface";
+import { IRepository, ISearchableRepository } from "../../../domain/repository/repository.interface";
+import { SearchParams } from "../../../domain/repository/search-params";
+import { SearchResult } from "../../../domain/repository/search-result";
 import { ValueObject } from "../../../domain/value-object";
 
-
-export abstract class InMemoryRepository<E extends Entity, EntityId extends ValueObject> implements IRepository<E, EntityId>{
+export abstract class InMemoryRepository<
+  E extends Entity,
+  EntityId extends ValueObject
+> implements IRepository<E, EntityId>{
   items: E[] = [];
 
   async insert(entity: E): Promise<void> {
@@ -26,7 +30,7 @@ export abstract class InMemoryRepository<E extends Entity, EntityId extends Valu
     const indexFound = this.items.findIndex(item => item.entity_id.equals(entity_id));
     if (indexFound === -1) throw new NotFoundError(entity_id, this.getEntity());
 
-    this.items.splice(indexFound, 1); 
+    this.items.splice(indexFound, 1);
   }
 
   async findById(entity_id: EntityId): Promise<E> {
@@ -37,6 +41,42 @@ export abstract class InMemoryRepository<E extends Entity, EntityId extends Valu
   async findAll(): Promise<E[]> {
     return this.items;
   }
-  
-  abstract getEntity(): new (...args: any[]) => E 
+
+  abstract getEntity(): new (...args: any[]) => E
+}
+
+export abstract class InMemorySearchableRepository<
+  E extends Entity,
+  EntityId extends ValueObject,
+  Filter = string
+> extends InMemoryRepository<E, EntityId>
+  implements ISearchableRepository<E, EntityId, Filter>{
+  sortableFields: string[] = [];
+  async search(props: SearchParams<Filter>): Promise<SearchResult<E>> {
+    const itemsFiltered = await this.applyFilter(this.items, props.filter);
+    const itemsSorted = this.applySort(itemsFiltered, props.sort, props.sort_dir);
+    const itemsPaginated = this.applyPaginate(itemsSorted, props.page, props.per_page);
+
+    return new SearchResult({
+      items: itemsPaginated,
+      total: itemsFiltered.length,
+      current_page: props.page,
+      per_page: props.per_page
+    })
+  }
+
+  protected abstract applyFilter(items: E[], filter: Filter | null): Promise<E[]>;
+
+  protected applyPaginate(
+    items: E[],
+    page: SearchParams['page'],
+    per_page: SearchParams['per_page']
+  ) {
+    const start = (page - 1) * per_page;
+    const limit = start + per_page;
+    
+    return items.slice(start, limit);
+  }
+
+  protected applySort() { }
 }
